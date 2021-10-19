0c6b735f1b020259a064479b5b61cacf0f582dc1
sandeep <sandeepkp601@gmail.com>
2021-10-19 20:33:18 +0530

    final

--- matri/settings.py
+++ matri/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-STATIC_ROOT = '/staticfiles/'
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 AUTH_USER_MODEL='app.Registration'
 
