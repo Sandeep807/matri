5f7260fa272247d7f1ae1ab1a45d617968bbd537
sandeep <sandeepkp601@gmail.com>
2021-10-21 12:29:11 +0530

    s

--- app/admin.py
+++ app/admin.py
@@ -1,16 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from django.contrib import admin
-from .models import *
-from taxi.models import *
+from app.models import *
+# from taxi.models import *
 # Register your models here.
 admin.site.register(Registration)
 admin.site.register(Package)
 admin.site.register(PaymentDetails)
 # admin.site.register(Payment)
 # admin.site.register(DriverRegistration)
-admin.site.register(Booking)
+# admin.site.register(Booking)
 # admin.site.register(BasicDetails)
 # admin.site.register(CasteDetails)
 # admin.site.register(PersonalDetails)
--- matri/settings.py
+++ matri/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'lxn##8_m%5zw&ezjy=3-t^+&+h1en6aiu^6_&(n$dg7*&rts_6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
