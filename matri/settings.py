1a30e5bcd55de58e981a742f0c2a4571286e25a5
sandeep <sandeepkp601@gmail.com>
2021-11-06 12:32:21 +0530

    sandeep

--- matri/settings.py
+++ matri/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'lxn##8_m%5zw&ezjy=3-t^+&+h1en6aiu^6_&(n$dg7*&rts_6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
