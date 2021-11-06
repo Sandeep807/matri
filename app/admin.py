a3d27a6292ff39aaef116e49ebce67c1609e4b2c
sandeep <sandeepkp601@gmail.com>
2021-11-06 13:52:29 +0530

    sandeep

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 admin.site.site_header = "Karthavyabharath"
 admin.site.index_title = "Welcome to Karthavyabharath Site"
 from .models import *
@@ -6,11 +8,13 @@ from .models import *
 @admin.register(Registration)
 class RegistrationAdmin(admin.ModelAdmin):
     list_display = ('id','first_name','last_name','mobile_number','email','password',
-                    'gender','profile_created_by','dob','religion','mother_tongue','caste',
+                    'gender','profile_created_by','dob','religion','mother_tongue','caste','gotra',
                     'dosh','height','marital_status','any_disability','family_status',
                     'family_type','family_value','education','employed_in','occupation',
                     'annual_income','work_location','residing_state','city',
-                    'pic','create_at','updated_at','otp')
+                    'image_customer','create_at','updated_at','otp')
+    def image_customer(self,obj):
+        return format_html(f'<img src="/media/{obj.pic}" style=height:50px;width:50px>')
 
 
 @admin.register(Package)
